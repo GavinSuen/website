895fff142556e1be45901bdfd9fd11816cd37c79
E.leven <2291646303@qq.com>
2017-04-29 21:14:51 +0800

    创新训练

--- 创新训练/code/js/index.js
+++ 创新训练/code/js/index.js
@@ -4,13 +4,56 @@
 	var imgs = document.querySelectorAll(".banner-pic");
 	var box = document.querySelector(".banner-box");
 	var btnUp = document.querySelector(".arraw-up");
-	var btnDn = document.querySelector(".arrae-dn");
+	var btnDn = document.querySelector(".arraw-dn");
+	var btnLi = document.querySelectorAll(".arraw-btn-list");
+	var posLeft
 
 	//点击上翻
 	btnUp.onclick=function(){
-		//var posLeft=0;
-		//posLeft -= 730; 
-		box.offsetLeft = box.offsetLeft - "730px";
-		alert(box.offsetLeft);
+		var posLeft= box.offsetLeft;
+		posLeft = posLeft + 730;
+		if(posLeft > 0){
+			box.style.left = -730*(imgs.length-1) + "px";
+		}else{
+			box.style.left = posLeft + "px";
+		}
+		activeImg();
 	};
+
+	//点击下翻
+	btnDn.onclick=function(){
+		var posLeft= box.offsetLeft;
+		posLeft = posLeft - 730;
+		if(posLeft > -730*(imgs.length)){
+			box.style.left = posLeft + "px";
+		}else{
+			box.style.left = 0 + "px";
+		}
+		activeImg();
+		
+	};
+
+	//按钮点击
+	for(var i=0;i<btnLi.length;i++){
+		btnLi[i].index = i;
+		btnLi[i].onclick=function(){
+			posLeft = this.index * -730;
+			box.style.left = posLeft + "px";
+			activeImg();
+		};
+	}
+
+	//判断图片
+	function activeImg(){
+		var posLeft= box.offsetLeft;
+		var num = Math.abs(posLeft/730);
+		for(var i=0;i<btnLi.length;i++){
+			btnLi[i].style.background="yellow";
+		}
+		btnLi[num].style.background="blue";
+	}
+	activeImg();
+
+
+
 })();
